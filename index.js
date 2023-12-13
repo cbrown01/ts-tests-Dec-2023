627f693a12f0586d3c32c4fc4da55d6276d97f70
cbrown01 <cbrown@themeetgroup.com>
2023-12-13 10:04:54 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const pkg = require('./package.json');
+const schema = require('./etc/schemas/config.json');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.send([pkg?.name, pkg?.version, schema?.title].join(' : '));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
